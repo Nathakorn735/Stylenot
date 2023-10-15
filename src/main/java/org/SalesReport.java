cce7547d799b05af15323815330739e9b433990e
Nathakorn Phikromsuk <6410210735@email.psu.ac.th>
2023-10-16 00:13:11 +0700

    total_day

--- src/main/java/org/SalesReport.java
+++ src/main/java/org/SalesReport.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SalesReport {
     private static final String RECEIPT_FILE = "src/resources/json/receipt.json";
@@ -35,11 +37,17 @@ public class SalesReport {
     private void displayReceiptData(JSONArray receipts) {
         double totalSales = 0;
 
+        // Map เพื่อเก็บยอด Quantity ของ Product ID แต่ละชิ้น
+        Map<String, Integer> productQuantityMap = new HashMap<>();
+        // Map เพื่อเก็บยอดขายตามวัน
+        Map<String, Double> dailySalesMap = new HashMap<>();
+
         // แสดงข้อมูลทั้งหมดจาก receipts
         for (Object receiptObj : receipts) {
             JSONObject receipt = (JSONObject) receiptObj;
+            String date = (String) receipt.get("date");
             System.out.println("Receipt ID: " + receipt.get("receiptId"));
-            System.out.println("Date: " + receipt.get("date"));
+            System.out.println("Date: " + date);
             System.out.println("Time: " + receipt.get("time"));
             System.out.println("Total Price: " + receipt.get("totalPrice"));
             System.out.println("Amount Received: " + receipt.get("amountReceived"));
@@ -53,9 +61,16 @@ public class SalesReport {
 
             for (Object productObj : products) {
                 JSONObject product = (JSONObject) productObj;
-                System.out.println("  Product ID: " + product.get("productID"));
-                System.out.println("  Product Name: " + product.get("productName"));
-                System.out.println("  Quantity: " + product.get("quantity"));
+                String productID = (String) product.get("productID");
+                String productName = (String) product.get("productName");
+                int quantity = Integer.parseInt(product.get("quantity").toString());
+
+                // บันทึกหรือเพิ่ม Quantity ใน Map
+                productQuantityMap.put(productID, productQuantityMap.getOrDefault(productID, 0) + quantity);
+
+                System.out.println("  Product ID: " + productID);
+                System.out.println("  Product Name: " + productName);
+                System.out.println("  Quantity: " + quantity);
                 System.out.println("  Color: " + product.get("color"));
                 System.out.println("  Price: " + product.get("price"));
                 System.out.println("  Total Price: " + product.get("totalPrice"));
@@ -63,10 +78,24 @@ public class SalesReport {
             }
 
             System.out.println("------------------------------");
+
+            // เพิ่มยอดขายลงใน Map ตามวัน
+            dailySalesMap.put(date, dailySalesMap.getOrDefault(date, 0.0) + receiptTotal);
+        }
+
+        // แสดง Product ID Summary
+        System.out.println("Product ID Summary:");
+        for (Map.Entry<String, Integer> entry : productQuantityMap.entrySet()) {
+            System.out.println("  Product ID: " + entry.getKey() + ", Quantity: " + entry.getValue());
+        }
+
+        // แสดง Total Sales ตามวัน
+        System.out.println("Daily Sales Summary:");
+        for (Map.Entry<String, Double> entry : dailySalesMap.entrySet()) {
+            System.out.println("  Date: " + entry.getKey() + ", Total Sales: " + entry.getValue());
         }
 
         System.out.println("Total Sales: " + totalSales);
         System.out.println("End of Receipt Data");
     }
-
 }
